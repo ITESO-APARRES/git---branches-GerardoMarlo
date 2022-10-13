9e2bd6b29461ed5294bef330578172a0eefa50f4
Luis Gerardo <gerardomartinez1999.gm@gmail.com>
2022-10-12 23:40:38 -0500

    Me equivoque de branch al crear los archivos math jij

--- math.c
+++
@@ -1,9 +0,0 @@
-/* Funciones matematicas para neustro codigo */
-
-int dobleInt(int a) {
-	if(a >MAX_INT) {
-		return -1;
-	}
-
-	return a*2;
-}
--- math.h
+++
@@ -1,5 +0,0 @@
-/* Encabezados de Math.c */
-
-#define MAX_INT (2^sizeof(nit))/2
-
-int dobleInt(int a);
